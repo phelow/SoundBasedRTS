403c449eee0e48b71d32a7247c083cca6667171e
phelow <wpheloung@gmail.com>
2016-10-31 17:44:39 -0400

    Procedurally generated soundtrack

---
+++ New Unity Project/Assets/ControlScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlScript : MonoBehaviour {
+
+	public Hv_ModifiedDemoPatch_LibWrapper HeavyScript;
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("space"))
+			
+		{
+			//HeavyScript.SendBangToReceiver ("onOff");
+			//print ("space pressed");
+		}
+		
+		else if (Input.GetKeyDown ("q"))
+
+		{
+			//HeavyScript.SendBangToReceiver ("waveToggle");
+			//print ("space pressed");
+
+		}
+
+		else if (Input.GetKey ("a"))
+			
+		{
+			HeavyScript.freq -= 2;
+			//print ("space pressed");
+		}
+
+		else if (Input.GetKey ("s"))
+			
+		{
+			HeavyScript.freq += 2;
+			//print ("space pressed");
+		}
+
+
+
+		else
+			{
+			return;
+			}
+		}
+
+}
+
+
